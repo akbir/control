71d1e8ae724f772d234b3a614ee37f3d6fb37aee
jiaxinwen <wenjx_thu@163.com>
2024-06-22 01:04:21 +0000

    update eval

--- src/eval.py
+++ src/eval.py
@@ -1,30 +1,102 @@
 import json
 import sys
 sys.path.append("/home/wenjiaxin/control")
-print(sys.path)
 
 from core.llm_api.llm import ModelAPI
 from collections import defaultdict
 import asyncio
 from core.utils import setup_environment
+from collections import Counter
+import numpy as np
+
+from src.exec.executor import batched_execute_code_test_case
 
 
+def get_message(prompt, item):
+    message = [
+        {"role": "system", "content": "You are an expert Python programmer."},
+        {"role": "user", "content": f"Solve this problem using python code only:\n{prompt['question']}"},
+        {"role": "assistant", "content": f"```python\n{prompt['solution']}\n```"},
+        {"role": "user", "content": f"Solve this problem using python code only:\n{prompt['question_2']}"},
+        {"role": "assistant", "content": f"```python\n{prompt['solution_2']}\n```"},
+        {"role": "user", "content": f"Solve this problem using python code only:\n{item['question']}"}
+    ]
+    return message
+
+
+def evaluate():
+    # save_path = 'results/apps_test.jsonl'
+    save_path = 'results/apps_test_gpt4-0613.jsonl'
+    data = []
+    with open(save_path) as f:
+        for line in f:
+            item = json.loads(line)
+            # question = item['prompt'][-1]['content'][0]['text'][len("Solve this problem using python code only:"):].strip()
+            question = item['prompt'][-1]['content'][len("Solve this problem using python code only:"):].strip()
+            gen = item['response'][0]['completion']
+            raw_gen = gen
+            try:
+                if "```python" in gen:
+                    gen = gen[gen.index("```python")+len("```python"):]
+                if "\n```" in gen:
+                    gen = gen[:gen.index("\n```")]
+                assert any(gen.strip())
+            except:
+                print(raw_gen)
+                print('='*100)
+                print(gen)
+                exit(0)
+            gen = gen.strip()
+            data.append({"question": question, "solution": gen})
+
+    print('size = ', len(data))
+    print(data[0])
+
+    res = batched_execute_code_test_case(data)
+
+    print('overall acc = ', np.mean([i['overall_flag'] for i in res]))
+    for difficulty in ['introductory', 'interview', 'competition']:
+        print(f'{difficulty} acc = ', np.mean([i['overall_flag'] for i in res if i['difficulty'] == difficulty]))
+
+    with open(save_path[:-1], 'w') as f:
+        json.dump(res, f, indent=2)
 
 async def main():
     # load data
-    in_path = ''
-    save_path = ''
+    prompt_file = 'prompts/code_gen.json'
+    in_path = '/home/wenjiaxin/control/data/APPS/test.json'
+
+    # model = "claude-3-5-sonnet-20240620"
+    # save_path = 'results/apps_test.jsonl'
+
+    model = 'gpt-4-0613'
+    save_path = 'results/apps_test_gpt4-0613.jsonl'
 
     message_list = []
+    with open(in_path) as f:
+        data = json.load(f)[:50]
+    print(Counter([i['difficulty'] for i in data]))
+    
+    with open(prompt_file) as f:
+        prompt = json.load(f)
+    for i in data:
+        message = get_message(prompt, i)
+        message_list.append(message)
+
+    # print example
+    # for i in message_list[0]:
+    #     print('='*50+i['role']+'='*50)
+        # print(i['content'])
+    # exit(0)
 
 
     # send request
-    model_api = ModelAPI(anthropic_num_threads=2, openai_fraction_rate_limit=0.99)
+    model_api = ModelAPI(anthropic_num_threads=10, openai_fraction_rate_limit=0.99)
     anthropic_requests = [
         model_api(
-            "claude-3-5-sonnet-20240620",
+            model,
             message,
-            max_tokens=1024,
+            max_tokens=4096,
             print_prompt_and_response=False,
             save_path=save_path,
             temperature=0,
@@ -50,4 +122,5 @@ async def main():
 
 if __name__ == '__main__':
     setup_environment()
-    asyncio.run(main())
+    # asyncio.run(main())
+    evaluate()
